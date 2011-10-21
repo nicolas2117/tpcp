26f7389717c4ec2b7a6c632e3dc9698a8c0c0ca0
Claude MM <claudemm@MacBook-Air-de-Claude.local>
2011-10-21 15:33:28 +0200

    [Table des identificateurs] Modification de l'interface

--- src/ding.cpp
+++ src/ding.cpp
@@ -1,7 +1,20 @@
-#include <cstdlib>
+#include <stdlib.h>
 #include <iostream>
+#include <TableDesIdentificateurs.hpp>
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
+    TableDesIdentificateurs TDI;
+    char s[50];
+    strcpy(s, "testa");
+    TDI.ajouter(s);
+    strcpy(s, "testb");
+    TDI.ajouter(s);
+    TDI.ajouter("tata");
+    TDI.afficher(std::cout);
+    int testId = TDI.getId("tata");
+    std::cout << "ID de tata : " << testId << std::endl;
+    std::cout << TDI.getNom(0) << std::endl;
     return EXIT_SUCCESS;
 }

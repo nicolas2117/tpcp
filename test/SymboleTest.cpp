9182c2e4e7dd614a8b9e508083446424d0a07eb6
nicolas2117 <nicolas2117@gmail.com>
2011-10-31 10:40:16 +0100

    Fichier de test des classes symbole*

--- test/SymboleTest.cpp
+++ test/SymboleTest.cpp
@@ -2,6 +2,12 @@
 #include <iostream>
 #include <Symbole.hpp>
 #include <SymboleProgramme.hpp>
+#include <SymboleConstante.hpp>
+#include <SymboleVariable.hpp>
+#include <SymboleParametre.hpp>
+#include <SymboleProcedure.hpp>
+#include <SymboleFonction.hpp>
+#include <TableDesSymboles.hpp>
 
 int main(int argc, char *argv[])
 {
@@ -10,6 +16,21 @@ int main(int argc, char *argv[])
 
     SymboleProgramme symboleProgramme(1);
     std::cout << "Id = " << symboleProgramme.getId() << " categorie = " << symboleProgramme.getCategorie() << std::endl;
+
+    SymboleVariable symboleVariable(1, new Type());
+    std::cout << "Id = " << symboleVariable.getId() << " categorie = " << symboleVariable.getCategorie() << std::endl;
+
+    SymboleConstante symboleConstante(1, new Type());
+    std::cout << "Id = " << symboleConstante.getId() << " categorie = " << symboleConstante.getCategorie() << std::endl;
+
+    SymboleParametre symboleParametre(1, new Type());
+    std::cout << "Id = " << symboleParametre.getId() << " categorie = " << symboleParametre.getCategorie() << std::endl;
+
+    SymboleFonction symboleFonction(1, new Type(), new TableDesSymboles());
+    std::cout << "Id = " << symboleFonction.getId() << " categorie = " << symboleFonction.getCategorie() << std::endl;
+
+    SymboleProcedure symboleProcedure(1, new TableDesSymboles());
+    std::cout << "Id = " << symboleProcedure.getId() << " categorie = " << symboleProcedure.getCategorie() << std::endl;
 
     return EXIT_SUCCESS;
 }

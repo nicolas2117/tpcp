a9a9613ceeeb98d1cffbc9d6a7d795ece7b5bf89
Maxime Mélinon <maxime@melinon.com>
2011-12-03 16:05:24 +0100

    get parent dans table des symboles

--- include/TableDesSymboles.hpp
+++ include/TableDesSymboles.hpp
@@ -15,47 +15,52 @@
 /**
  * @brief Classe TableDesSymboles.
  */
-class TableDesSymboles
-{
-    private:
-        std::vector<Symbole *> symboles; /**< Liste des symboles. */
-        const TableDesSymboles *parent; /**< Pointeur vers la table des symboles parent. */
+class TableDesSymboles {
+private:
+    std::vector<Symbole *> symboles; /**< Liste des symboles. */
+    const TableDesSymboles *parent; /**< Pointeur vers la table des symboles parent. */
 
-    public:
-        /**
-         * @brief constructeur.
-         */
-        TableDesSymboles();
+public:
+    /**
+     * @brief constructeur.
+     */
+    TableDesSymboles();
 
-        /**
-         * @brief constructeur.
-         * @param parent Pointeur vers la table des symboles parent.
-         */
-        TableDesSymboles(const TableDesSymboles *parent);
+    /**
+     * @brief constructeur.
+     * @param parent Pointeur vers la table des symboles parent.
+     */
+    TableDesSymboles(const TableDesSymboles *parent);
 
-        /**
-         * @brief Destructeur.
-         */
-        ~TableDesSymboles();
+    /**
+     * @brief Destructeur.
+     */
+    ~TableDesSymboles();
 
-        /**
-         * @brief Ajoute un symbole à la table.
-         * @param symbole Un symbole.
-         */
-        void ajouterSymbole(Symbole *symbole);
+    /**
+     * @brief Ajoute un symbole à la table.
+     * @param symbole Un symbole.
+     */
+    void ajouterSymbole(Symbole *symbole);
 
-        /**
-         * @brief Retourne un pointeur vers un symbole.
-         * @param id L'id du symbole que l'on veut récupèrer.
-         * @return Un pointeur vers un symbole.
-         */
-        const Symbole *getSymbole(int id) const;
+    /**
+     * @brief Retourne un pointeur vers un symbole.
+     * @param id L'id du symbole que l'on veut récupèrer.
+     * @return Un pointeur vers un symbole.
+     */
+    const Symbole *getSymbole(int id) const;
 
-        /**
-         * @brief Affiche la table des symboles.
-         * @param flux Le flux sur lequel afficher la table.
-         */
-        void afficher(std::ostream &flux);
+    /**
+     * @brief Affiche la table des symboles.
+     * @param flux Le flux sur lequel afficher la table.
+     */
+    void afficher(std::ostream &flux);
+
+    /**
+     * @brief Retourne la table des symboles parents
+     * @param la table des symboles du parent
+     */
+    const TableDesSymboles* getParent();
 };
 
 #endif
--- src/TableDesSymboles.cpp
+++ src/TableDesSymboles.cpp
@@ -2,49 +2,46 @@
 #include <iostream>
 #include <TableDesSymboles.hpp>
 
-TableDesSymboles::TableDesSymboles()
-{
+TableDesSymboles::TableDesSymboles() {
     this->parent = NULL;
 }
 
-TableDesSymboles::TableDesSymboles(const TableDesSymboles *parent)
-{
+TableDesSymboles::TableDesSymboles(const TableDesSymboles *parent) {
     this->parent = parent;
 }
 
-TableDesSymboles::~TableDesSymboles()
-{
+TableDesSymboles::~TableDesSymboles() {
 
 }
 
-void TableDesSymboles::ajouterSymbole(Symbole *symbole)
-{
+void TableDesSymboles::ajouterSymbole(Symbole *symbole) {
     symboles.push_back(symbole);
 }
 
-const Symbole *TableDesSymboles::getSymbole(int id) const
-{
+const Symbole *TableDesSymboles::getSymbole(int id) const {
     unsigned int i;
-    for(i = 0; i < symboles.size(); i++)
-    {
-        if(symboles[i]->getId() == id)
+    for (i = 0; i < symboles.size(); i++) {
+        if (symboles[i]->getId() == id)
             return symboles[i];
     }
-	if (parent == NULL) 
-	{
-		return NULL;
-	}
-	else
-	{
-		return parent->getSymbole(id);
-	}
-}
-
-void TableDesSymboles::afficher(std::ostream &flux)
-{
-    unsigned int i;
-    for(i = 0; i < symboles.size(); i++)
-    {
-        std::cout << symboles[i]->getId() <<  " " << symboles[i]->getCategorie() << std::endl;
+    if (parent == NULL) {
+        return NULL;
+    } else {
+        return parent->getSymbole(id);
     }
 }
+
+void TableDesSymboles::afficher(std::ostream &flux) {
+    unsigned int i;
+    flux << "+++SymbolTable(" << 0 << ":" << 0 << ")" << std::endl;
+    for (i = 0; i < symboles.size(); i++) {
+        std::cout << symboles[i]->getId() << " " << symboles[i]->getCategorie() << std::endl;
+    }
+    flux << "---SymbolTable" << std::endl;
+}
+
+const TableDesSymboles* TableDesSymboles::getParent() {
+    return this->parent;
+}
+
+

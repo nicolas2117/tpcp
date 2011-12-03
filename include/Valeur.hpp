b2bcdf8374eced05663c670f15caf9f85241bb25
Maxime Mélinon <maxime@melinon.com>
2011-12-03 14:37:13 +0100

    Valeur.cpp/hpp

---
+++ include/Valeur.hpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   Valeur.hpp
+ * Author: Maxime Mélinon
+ *
+ * Created on 18 novembre 2011, 16:05
+ */
+
+#ifndef VALEUR_HPP
+#define	VALEUR_HPP
+
+class Valeur {
+public:
+    Valeur();
+    Valeur(const Valeur& orig);
+    virtual ~Valeur();
+private:
+
+};
+
+#endif	/* VALEUR_HPP */
+
---
+++ src/Valeur.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   Valeur.cpp
+ * Author: Maxime Mélinon
+ * 
+ * Created on 18 novembre 2011, 16:05
+ */
+
+#include "Valeur.hpp"
+
+Valeur::Valeur() {
+}
+
+Valeur::Valeur(const Valeur& orig) {
+}
+
+Valeur::~Valeur() {
+}
+
